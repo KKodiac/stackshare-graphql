18338b4e8bfd28ff79c5fea095b31246cdf0e058
Ubuntu <ubuntu@ip-172-31-26-15.ap-northeast-2.compute.internal>
2022-01-09 07:04:16 +0000

    Remove clutter

--- android/app/src/main/java/com/example/stacklounge/board/BoardCommentData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.stacklounge.board
-
-//댓글 data
-data class BoardCommentData(
-    var userId : String = "",
-    var boardCommment : String= "",
-    var commentTime : String= "",
-    var userphoto : String= ""
-)
--- android/app/src/main/java/com/example/stacklounge/query/Client.kt
+++
@@ -1,8 +0,0 @@
-package com.example.stacklounge.query
-
-import android.content.Context
-import com.apollographql.apollo.ApolloClient
-
-val apolloClient = ApolloClient.builder()
-    .serverUrl("http://15.164.18.220:8000/graphql")
-    .build()
